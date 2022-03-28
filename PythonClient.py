acce8a30986bc6050085c2455b7babaeba634d50
syx2259 <syx2259@gmail.com>
2022-03-27 20:33:29 -0400

    Update PythonClient.py

--- PythonClient.py
+++ PythonClient.py
@@ -6,11 +6,13 @@
 # asynchronousJob() : which calls a "oneway" method
 #
 
-host = "localhost"
-port = 9090
+# host = "localhost"
+# port = 9090
 
+from asyncio import futures
 import sys
 import time
+from concurrent.futures import ThreadPoolExecutor
 
 # your gen-py dir
 sys.path.append('../gen-py')
@@ -25,39 +27,81 @@ from thrift.transport import TSocket
 from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
 
-try:
 
-    # Init thrift connection and protocol handlers
-    transport = TSocket.TSocket( host , port)
+def requestOne(host, port, index):
+    transport = TSocket.TSocket(host, port)
     transport = TTransport.TBufferedTransport(transport)
     protocol = TBinaryProtocol.TBinaryProtocol(transport)
-
-    # Set client to our Example
     client = Example.Client(protocol)
-
-    # Connect to server
     transport.open()
-
-    # startTime of RTT
-    startTime = time.time()
-    
-    # Run showCurrentTimestamp() method on server
-    currentTime = client.showCurrentTimestamp()
-    
-    # Calculate the RTT
-    endTime = time.time()
-    print("RTT: " + str(endTime - startTime))
-    print("Request result: " + currentTime)
-
-    # Assume that you have a job which takes some time
-    # but client sholdn't have to wait for job to finish
-    # ie. Creating 10 thumbnails and putting these files to sepeate folders
-    client.asynchronousJob()
-
-
-    # Close connection
+    if index == 0:
+        f = open("rtt.txt", "w")
+        for x in range(10000):
+            startTime = time.time()
+            # Run showCurrentTimestamp() method on server
+            currentTime = client.showCurrentTimestamp()
+            endTime = time.time()
+            interval = endTime - startTime
+            f.write(str(interval) + "\n")
+        f.close()
+    else :
+        # Run showCurrentTimestamp() method on server
+        currentTime = client.showCurrentTimestamp()
     transport.close()
+    return "Finish work in " + host
 
-except Thrift.TException as tx:
-    print('Something went wrong : %s' % (tx.message))
+def requestAll(hosts):
+    futures_list = []
+    results = []
+    with ThreadPoolExecutor(max_workers=6) as executor:
+        index = 0
+        for host in hosts:
+            futures = executor.submit(requestOne, [host, 9090, index])
+            futures_list.append(futures)
+    
+        for future in futures_list:
+            try:
+                result = future.result(timeout=60)
+                results.append(result)
+            except Exception:
+                results.append(None)
+    return results
 
+if __name__ == "__main__":
+    try:
+        hosts = ("10.0.2.21", "10.0.2.22", "10.0.2.23", "10.0.2.24", "10.0.2.25")
+        results = requestAll(hosts)
+        for result in results:
+            print(result)
+        # Init thrift connection and protocol handlers
+        # transport = TSocket.TSocket( host , port)
+        # transport = TTransport.TBufferedTransport(transport)
+        # protocol = TBinaryProtocol.TBinaryProtocol(transport)
+
+        # # Set client to our Example
+        # client = Example.Client(protocol)
+
+        # # Connect to server
+        # transport.open()
+
+        # # startTime of RTT
+        # startTime = time.time()
+        
+        # # Run showCurrentTimestamp() method on server
+        # currentTime = client.showCurrentTimestamp()
+        
+        # # Calculate the RTT
+        # endTime = time.time()
+        # print("RTT: " + str(endTime - startTime))
+        # print("Request result: " + currentTime)
+
+        # Assume that you have a job which takes some time
+        # but client sholdn't have to wait for job to finish
+        # ie. Creating 10 thumbnails and putting these files to sepeate folders
+        #client.asynchronousJob()
+
+
+        # Close connection
+        # transport.close()
+    except Thrift.TException as tx:
+        print('Something went wrong : %s' % (tx.message))
