8d8af36bfef64254233a518476508e39c19434b0
syx2259 <syx2259@gmail.com>
2022-04-23 18:25:01 -0400

    rm

--- thrift/Example.thrift
+++
@@ -1,8 +0,0 @@
-namespace php Example
-service Example{
-    // return current time stamp
-    string showCurrentTimestamp()
-     
-    // wait for 10 seconds, but work asynchronously
-    oneway void asynchronousJob()
-}
